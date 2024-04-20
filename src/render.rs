6b7918fa578e66125892fc388e817700dbdcd1e7
Karthik-S-Salian <karthikssalian5@gmail.com>
2024-04-20 20:06:54 +0530

    init rust

---
+++ src/render.rs
@@ -0,0 +1,170 @@
+use bytemuck::{Pod, Zeroable};
+
+use crate::{HEIGHT, WIDTH};
+
+pub struct PathTracer {
+    device: wgpu::Device,
+    queue: wgpu::Queue,
+
+    display_pipeline: wgpu::RenderPipeline,
+
+    uniforms: Uniforms,
+    uniform_buffer: wgpu::Buffer,
+
+    display_bind_group: wgpu::BindGroup,
+}
+
+#[derive(Copy, Clone, Pod, Zeroable)]
+#[repr(C)]
+struct Uniforms {
+    width: u32,
+    height: u32,
+}
+
+impl PathTracer {
+    pub fn new(device: wgpu::Device, queue: wgpu::Queue,width:u32,height:u32) -> PathTracer {
+        device.on_uncaptured_error(Box::new(|error| {
+            panic!("Aborting due to error: {}", error);
+        }));
+
+        let shader_module = compile_shader_module(&device);
+        let (display_pipeline, display_layout) = create_display_pipeline(&device, &shader_module);
+
+        let uniforms = Uniforms {
+            width,
+            height,
+        };
+
+        let uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+            label: Some("uniforms"),
+            size: std::mem::size_of::<Uniforms>() as u64,
+            usage: wgpu::BufferUsages::UNIFORM,
+            mapped_at_creation: true,
+        });
+
+        uniform_buffer
+            .slice(..)
+            .get_mapped_range_mut()
+            .copy_from_slice(bytemuck::bytes_of(&uniforms));
+
+        uniform_buffer.unmap();
+
+        let display_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+            label: None,
+            layout: &display_layout,
+            entries: &[wgpu::BindGroupEntry {
+                binding: 0,
+                resource: wgpu::BindingResource::Buffer(wgpu::BufferBinding {
+                    buffer: &uniform_buffer,
+                    offset: 0,
+                    size: None,
+                }),
+            }],
+        });
+
+        PathTracer {
+            device,
+            queue,
+            display_pipeline,
+            uniforms,
+            uniform_buffer,
+            display_bind_group
+        }
+    }
+
+    pub fn render_frame(&self, target: &wgpu::TextureView) {
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("render frame"),
+            });
+
+        let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+            label: Some("display pass"),
+            color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                view: target,
+                resolve_target: None,
+                ops: wgpu::Operations {
+                    load: wgpu::LoadOp::Clear(wgpu::Color::BLACK),
+                    store: wgpu::StoreOp::Store,
+                },
+            })],
+            ..Default::default()
+        });
+
+        render_pass.set_pipeline(&self.display_pipeline);
+        render_pass.set_bind_group(0, &self.display_bind_group, &[]);
+
+        render_pass.draw(0..6, 0..1);
+
+        // End the render pass by consuming the object.
+        drop(render_pass);
+
+        let command_buffer = encoder.finish();
+        self.queue.submit(Some(command_buffer));
+    }
+}
+
+fn compile_shader_module(device: &wgpu::Device) -> wgpu::ShaderModule {
+    use ::std::borrow::Cow;
+
+    let code = include_str!(concat!(env!("CARGO_MANIFEST_DIR"), "/src/shaders.wgsl"));
+    device.create_shader_module(wgpu::ShaderModuleDescriptor {
+        label: None,
+        source: wgpu::ShaderSource::Wgsl(Cow::Borrowed(code)),
+    })
+}
+
+fn create_display_pipeline(
+    device: &wgpu::Device,
+    shader_module: &wgpu::ShaderModule,
+) -> (wgpu::RenderPipeline, wgpu::BindGroupLayout) {
+    let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+        label: None,
+        entries: &[wgpu::BindGroupLayoutEntry {
+            binding: 0,
+            visibility: wgpu::ShaderStages::FRAGMENT,
+            ty: wgpu::BindingType::Buffer {
+                ty: wgpu::BufferBindingType::Uniform,
+                has_dynamic_offset: false,
+                min_binding_size: None,
+            },
+            count: None,
+        }],
+    });
+
+    let pipeline = device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+        label: Some("display"),
+        layout: Some(
+            &device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
+                bind_group_layouts: &[&bind_group_layout],
+                ..Default::default()
+            }),
+        ),
+        vertex: wgpu::VertexState {
+            module: shader_module,
+            entry_point: "display_vs",
+            buffers: &[],
+        },
+        primitive: wgpu::PrimitiveState {
+            topology: wgpu::PrimitiveTopology::TriangleList,
+            front_face: wgpu::FrontFace::Ccw,
+            polygon_mode: wgpu::PolygonMode::Fill,
+            ..Default::default()
+        },
+        depth_stencil: None,
+        multisample: wgpu::MultisampleState::default(),
+        fragment: Some(wgpu::FragmentState {
+            module: shader_module,
+            entry_point: "display_fs",
+            targets: &[Some(wgpu::ColorTargetState {
+                format: wgpu::TextureFormat::Bgra8Unorm,
+                blend: None,
+                write_mask: wgpu::ColorWrites::ALL,
+            })],
+        }),
+        multiview: None,
+    });
+
+    (pipeline, bind_group_layout)
+}
---
+++ src/shaders.wgsl
@@ -0,0 +1,58 @@
+alias TriangleVertices = array<vec2f, 6>;
+var<private> vertices: TriangleVertices = TriangleVertices(
+  vec2f(-1.0,  1.0),
+  vec2f(-1.0, -1.0),
+  vec2f( 1.0,  1.0),
+  vec2f( 1.0,  1.0),
+  vec2f(-1.0, -1.0),
+  vec2f( 1.0, -1.0),
+);
+
+@vertex fn display_vs(@builtin(vertex_index) vid: u32) -> @builtin(position) vec4f {
+  return vec4f(vertices[vid], 0.0, 1.0);
+}
+
+//********************************************************************************//
+
+
+struct Uniforms {
+  width: u32,
+  height: u32,
+}
+
+struct Sphere {
+  center: vec3f,
+  radius: f32,
+}
+
+@group(0) @binding(0) var<uniform> uniforms: Uniforms;
+
+struct Ray {
+  origin: vec3f,
+  direction:vec3f,
+}
+
+fn sky_color(ray: Ray) -> vec3f {
+
+  //https://www.w3.org/TR/WGSL/#mix-builtin
+  let t = 0.5 * (normalize(ray.direction).y + 1.);
+  return mix(vec3(1.,.5,0.3),vec3(0.3, 0.5, 1.),t);
+}
+
+@fragment fn display_fs(@builtin(position) pos: vec4f) -> @location(0) vec4f {
+  let origin = vec3(0.);
+  let focus_distance = 1.;
+
+  let aspect_ratio = f32(uniforms.width)/f32(uniforms.height);
+
+  // Normalize the viewport coordinates.
+  var uv = pos.xy / vec2f(f32(uniforms.width - 1u), f32(uniforms.height - 1u));
+
+  // Map `uv` from y-down (normalized) viewport coordinates to camera coordinates.
+  uv = (2. * uv - vec2(1.)) * vec2(aspect_ratio, -1.);
+
+  let direction = vec3(uv, -focus_distance);
+  let ray = Ray(origin, direction);
+
+  return vec4(sky_color(ray), 1.);
+}
